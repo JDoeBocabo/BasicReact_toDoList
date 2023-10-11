8dca05e87e0ef7e6e387363f9d01dc8e73127bf8
JDoeBocabo <100052228+JDoeBocabo@users.noreply.github.com>
2023-10-11 17:48:06 +0800

    added a mark as done functionality

--- src/App.js
+++ src/App.js
@@ -12,27 +12,42 @@ function App() {
   const addList = () => {
     let newList = {
       id: toDoList.length === 0 ? 1 : toDoList[toDoList.length - 1].id + 1,
-      list: list
+      list: list,
+      completed: false
     } 
     settoDoList([...toDoList, newList])
   }
-
   const deleteList = (id) => {
     let reducedList = toDoList.filter((li) =>{
       return li.id !== id
     })
     settoDoList(reducedList)
   }
+
+    const markDone = (id) => {
+    settoDoList(
+      toDoList.map((li) => {
+        if (li.id === id) {
+          console.log(li.completed)
+          return { ...li, completed: li.completed ? false : true};
+        } else {
+          return li;
+        }
+      })
+    );
+  };
+
   return (
     <div className="App">
       <h1>To Do List</h1>
-      <div>
+      <div className='form'> 
         <input placeholder='add task' type="text" onChange={setToDo} />
         <button className="add" onClick={addList}>Add</button>
       </div>
+      <hr />
       <div className='list-container'>
         {toDoList.map((toDo, key) => {
-          return <List  list={toDo.list} id={toDo.id} key={key} deleteList={deleteList}/>
+          return <List  completed={toDo.completed} markDone={markDone} list={toDo.list} id={toDo.id} key={key} deleteList={deleteList}/>
         })}
       </div>
     </div>
--- src/List.js
+++ src/List.js
@@ -1,8 +1,9 @@
 export const List = (props) => {
     return (
-          <div className="list">
-            <div className="list-item"> <p>{props.id}. {props.list}</p> </div>
-            <button className="delete" onClick={() => props.deleteList(props.id)}>Done</button>
+          <div className="list" >
+            <div className="list-item"> <p > {props.id}. </p> <p style={{textDecorationLine : props.completed ? 'line-through' : 'none'}}> {props.list}</p> </div>
+            <button className="markDone" onClick={() => props.markDone(props.id)}>Done</button>
+            <button className="delete" onClick={() => props.deleteList(props.id)}>Delete</button>
           </div>
     )
 }
